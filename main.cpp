03c5d04928d1eeab2855a63e594a6a0a6cba82dd
Noah Lampron <noahlampron@gmail.com>
2019-03-22 13:16:42 -0400

    Add 3.4.5 example

--- main.cpp
+++ main.cpp
@@ -33,6 +33,40 @@ int FirstQuickSort(int a[5],int i, int j){
 	
 }
 */
+
+template <class T>
+void partition(T a[], int start, int stop, int & pivot) {
+	std::swap(a[pivot], a[start]);
+		//puts pivot in fist position
+	T * temp = new T[stop - start];
+	int k = 0;
+	for (int i = start + 1; i < stop; ++i)
+		if (a[i] < a[start]) {
+			temp[k] = a[i];
+			++k;
+		}
+	temp[k] = a[start];
+	pivot = start + k;
+	++k;
+	for (int i = start + 1; i < stop; ++i)
+		if (!(a[i] < a[start])){
+			temp[k] = a[i];
+			++k;
+		}
+	std::copy(temp, temp + k, a + start);
+	delete temp;
+}
+
+template <class T>
+void quicksort (T a[], int start, int stop) {
+	if (stop - start > 1) {
+		int pivot = a[start];
+		partition(a, start, stop, pivot);
+		quicksort(a, start, pivot);
+		quicksort(a, pivot + 1, stop);
+	}
+}
+
 int main(){
 	int a[5] = {29, 92, 38, 48, 11};
 	insertion_sort(a,0,6);
