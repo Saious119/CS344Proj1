7d4f9c9c1547911206b5e8194e4f0e934929e7cf
BoneKing <mahonea@clarkson.edu>
2019-04-03 13:21:10 -0400

    Problem 5 compiles

--- integerClass.cpp
+++ integerClass.cpp
@@ -4,25 +4,41 @@ using namespace std;
 
 class Integer{
 public:
-	void defaultConstructor(){ this->Integer_value =0;}
-	void IntegerConstructor(int x){this->Integer_value=x;}
-	void copy(Integer Other){this->Integer_count=Other.Integer_count;Integer_count++;}
-	int value(){return Integer_count;}
-	const bool Integer& operator <(const Integer& y){
+	void defaultConstructor(){ 
+		this->Integer_value =0;
+	}
+	void IntegerConstructor(int x){
+		this->Integer_value=x;
+	}
+	void copy(Integer Other){
+		this->Integer_value=Other.Integer_value;
 		this->Integer_count++;
-		int yval=y.value();
-		if(this->value()<y.value()){return true;}
+	}
+	const int value(){
+		return this->Integer_value;
+	}
+	void increment(){
+		Integer_count++;
+	}
+	bool operator<(const Integer & y)const{
+		//IntegerConstructor((Integer_count)+1);
+		//Integer_count++;
+		//this->increment();
+		//const int yval=y.value();
+		if(Integer_value<y.Integer_value){return true;}
 		else{return false;}
 	}
 	Integer& operator =(Integer& y){
-		this->Integer_count++;
-		this->value()=y.value();
+		//this->Integer_count++;
+		//this->value()=y.value();
+		this->copy(y);
 	}
 
 private:
 	int Integer_count = 0;
 	int Integer_value;
 };
+
 /*
 const bool Integer::operator < (const Integer &x, const Integer &y){
 	this->Integer_count++;
