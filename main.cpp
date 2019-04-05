39f149c203073f9fee1d2726dfe5957673d6600d
BoneKing <mahonea@clarkson.edu>
2019-04-05 11:12:49 -0400

    Integer class now in main

--- main.cpp
+++ main.cpp
@@ -7,6 +7,44 @@
 #include<cstdlib>
 
 using namespace std;
+
+class Integer{
+public:
+	void defaultConstructor(){ 
+		this->Integer_value =0;
+	}
+	void IntegerConstructor(int x){
+		this->Integer_value=x;
+	}
+	void copy(Integer Other){
+		this->Integer_value=Other.Integer_value;
+		this->Integer_count++;
+	}
+	const int value(){
+		return this->Integer_value;
+	}
+	void increment(){
+		Integer_count++;
+	}
+	bool operator<(const Integer & y)const{
+		//IntegerConstructor((Integer_count)+1);
+		//Integer_count++;
+		//this->increment();
+		//const int yval=y.value();
+		if(Integer_value<y.Integer_value){return true;}
+		else{return false;}
+	}
+	Integer& operator =(Integer& y){
+		//this->Integer_count++;
+		//this->value()=y.value();
+		this->copy(y);
+	}
+
+private:
+	int Integer_count = 0;
+	int Integer_value;
+};
+
 int insertion_sort(int a[5],int i,int j){
 	for(int k=i+1;k<(j-1);k++){
 		int x = a[k];
